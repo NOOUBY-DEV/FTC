c3d6831c85893e950921b1268e53dc4e90c8af39
NOOUBY-DEV <NOOUBY.DEV@gmail.com>
2026-06-29 14:19:41 +0800

    [&] add : ftc roadrunner files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RED_FRONT_1_DATA.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
+import org.firstinspires.ftc.onbotjava.handlers.file.NewFile;
+
+public class RED_FRONT_1_DATA
+{
+
+        public static Pose2d STARTING_POSE = new Pose2d(24, 24, Math.toRadians(45));
+
+        public static Vector2d A1 = new Vector2d(0, -12);
+
+
+        public static Pose2d FLIP_POSE(final Pose2d POSE)
+        {
+
+                return new Pose2d(-POSE.position.x, POSE.position.y, -POSE.heading.toDouble());
+
+        }
+
+        public final Vector2d FLIP_VEC(final Vector2d VECTOR)
+        {
+
+                return new Vector2d(-VECTOR.x, VECTOR.y);
+
+        }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_AUTO.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.RED_FRONT_1_DATA.A1;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.RED_FRONT_1_DATA;
+
+
+
+@Autonomous(name = "AUTO_TEST_JEGGY", group = "VORTEX")
+
+public class TEST_AUTO extends LinearOpMode
+{
+
+        private MecanumDrive DRIVE;
+
+        private final Pose2d STARTING = RED_FRONT_1_DATA.STARTING_POSE;
+
+
+                @Override
+        public void runOpMode()
+        {
+
+                INIT_ODOMETRY_AND_MECANUM();
+
+
+                telemetry.addData("&[EVENT]", "[INIT OK.]");
+                telemetry.update();
+
+
+                waitForStart();
+
+
+                Actions.runBlocking(DRIVE.actionBuilder(STARTING)
+
+                        .strafeTo(new Vector2d(0, 0))
+
+                        .strafeToLinearHeading(A1, 0)
+
+                        .build()
+                );
+
+        }
+
+
+        private void INIT_ODOMETRY_AND_MECANUM()
+        {
+
+                DRIVE = new MecanumDrive(hardwareMap, STARTING);
+
+        }
+
+}
